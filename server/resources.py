8fe29cb88458c953b7e58ff92b8585f3ef49b754
frejaKahle <frejakahle@gmail.com>
2023-04-18 14:57:53 -0400

    fix a bug regarding empty messages, add comments

--- server/resources.py
+++ server/resources.py
@@ -1,5 +1,9 @@
 import json
 
+# table that holds address:username pairs
+user_addr_table = {}
+
+# class that handles most logic for server
 class Message_Board():
     boards = []
     user_table = {} # username: send_data_user()
@@ -18,12 +22,14 @@ class Message_Board():
         return [self.id,self.name]
     def __repr__(self):
         return str(self.list)
+    # adds a user to this board
     def add_connection(self,username):
         if username in self.users:
             raise ValueError("User already connected to board")
         else:
             self.users.append(username)
             self.update_users()
+    # removes a user from this board
     def disconnect(self,username):
         if username in self.users:
             self.users.remove(username)
@@ -31,46 +37,52 @@ class Message_Board():
             self.update_users()
         else:
             raise ValueError("Specified user not connected to board")
+    # adds a message to this board
     def add_message(self,message,user):
         if message not in self._messages:
             self._messages[1] = self._messages [0]
             self._messages[0] = message
             Message_Board.user_table[user]({"MessageType":"Message","Success":True})
             self.update_users()
-    def get_update_message(self):
-        return {"MessageType":"Update",
+    # updates all users connected to this board when necessary
+    def update_users(self):
+        um = {"MessageType":"Update",
                 "Messages": self.messages,
                 "ConnectedUsers": self.users,
                 "MessageBoard": self.id}
-    def update_users(self):
-        um = self.get_update_message()
         for user in self.users:
             Message_Board.user_table[user](um)
+    # checks if a specific user is connected to any board in cls.boards
     @classmethod
     def user_connected(cls,user):
         for board in cls.boards:
             if user in board.users:
                 return True
         return False
+    # adds a username:user_function pair to the cls.user_table 
     @classmethod
     def add_user_table(cls,user,function):
         cls.user_table.update({user:function})
+    # remove a username:user_function pair from the cls.user_table if that user is not connected to any board
     @classmethod
     def remove_user_table(cls,user):
         if cls.user_connected(user):
             return    
-        cls.user_table.remove(user)
+        del cls.user_table[user]
+    # returns the get groups message response
     @classmethod
     def get_groups_message(cls):
         return {"MessageType":"Groups",
                 "Groups":[board.list for board in cls.boards]}
+    # acts as a table that is accessible via the class, allows the boards to be accessed via id or name
     @classmethod
     @property
     def boards_dict(cls):
         b = {board.id:board for board in cls.boards}
         b.update({board.name:board for board in cls.boards})
         return b
-        
+
+# initializes all 6 normal message boards
 Message_Board.boards = [
     Message_Board(0,"public"),
     Message_Board(1,"Private Room 1"),
@@ -79,6 +91,7 @@ Message_Board.boards = [
     Message_Board(4,"VIP Room"),
     Message_Board(5,"VIP Room 2")]
 
+# adds a user to a board and to the Message_Board user function table
 def add_user_connection(user,userfunc,board):
     b = Message_Board.boards_dict[board]
     Message_Board.add_user_table(user,userfunc)
@@ -87,6 +100,7 @@ def add_user_connection(user,userfunc,board):
               "Success":True,
               "MessageBoard":b.list})
 
+# removes a user from a board and from the Message_Board user function table provided they are no longer in any boards
 def end_user_connection(user,userfunc,board):
     b = Message_Board.boards_dict[board]
     b.disconnect()
@@ -94,7 +108,8 @@ def end_user_connection(user,userfunc,board):
     userfunc({"MessageType":"Disconnect",
               "Success":True,
               "MessageBoard":board})
-    
+
+# removes a user from all boards and from the Message_Board user function table
 def end_user_connection_all(user,userfunc):
     for board in Message_Board.boards:
         if user in board.users:
@@ -104,14 +119,14 @@ def end_user_connection_all(user,userfunc):
               "Success":True,
               "MessageBoard":None})
 
-
+# handles logic for determining if a user can send a message in a specific board
 def handle_user_message(username,userfunc,board,content):
     b = Message_Board.boards_dict[board]
     if username not in b.users:
         return userfunc({"MessageType":"Message","Success":False})
     b.add_message(content,username)
 
-
+# handles logic for determining what response the server should have for a given client message
 def handle_client_tcp_message(data,userfunc):
     {   "Connection":   lambda x: add_user_connection(x["Username"],userfunc,x["MessageBoard"]),
         "Message":      lambda x: handle_user_message(x["Username"],userfunc,x["MessageBoard"],x["Content"]),
--- server/server.py
+++ server/server.py
@@ -1,20 +1,44 @@
 import socket, json, asyncio
 from resources import *
 
+# table holding address:async_function pairs 
 connections = {}
-user_table = {}
 
+# function listening socket runs
 async def wait_for_connection(s):
+    # Loop through waiting for TCP connections
     while True:
+        # wait for connection and get socket (conn) and address of connection
         conn, addr = s.accept()
+        # initializes a function that will be called when sending data to the connected socket
         def connfunc(data):
             conn.send(bytes(json.dumps(data), 'UTF-8'))
+        # adds 
         connections.update({addr:handle_connection(conn,addr,connfunc)})
 
+# function each connected socket runs
 async def handle_connection(conn,addr,func):
+    # Loop through waiting for client messages
     while True:
+        # Wait for message from client and convert raw data to python dictionary
         data = json.loads(conn.recv(1024).decode("utf-8"))
-        if not data: data = {"MessageType":"Disconnect","Username":user_table[addr],"MessageBoard":None}
+
+        # if no content was sent, dissconnect user and close tcp socket
+        if not data:
+            data = {"MessageType":"Disconnect","Username":user_addr_table[addr],"MessageBoard":None}
+            del user_addr_table[addr]
+            del connections[addr]
+            break
+
+        # if user is disconnecting from all groups, but not closing the socket 
+        if data["MessageType"] == "Disconnect" and not data["MessageBoard"]:
+            del user_addr_table[addr]
+
+        # if user is sending a message that contains their username, add it to the user_addr_table for later use
+        if data["MessageType"] != "Groups":
+            user_addr_table[addr] = data["Username"]
+
+        # send data to resource handler to handle message logic
         handle_client_tcp_message(data,func)
         
 
@@ -22,7 +46,6 @@ port = int(input("Port?\n"))
 while(port < 1023 or port > 65536):
     port = int(input("Port? (1024-65536)\n"))
 
-connections = {}
 
 s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("", port))
